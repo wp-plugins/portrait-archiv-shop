89ed97904dc605461c8a8a0c2582b5a784b70e99
tschiffler <tschiffler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-23 10:46:26 +0000

    Erweiterungen
    git-svn-id: https://plugins.svn.wordpress.org/portrait-archiv-shop/tags/1.4.1@827390 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ portraitarchiv-store.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+Plugin Name: Portrait-Archiv.com Photostore
+Plugin URI: http://wordpress.org/plugins/portrait-archiv-shop/
+Description: Der Portrait-Archiv.com Photostore stellt dem Benutzer die Moeglichkeit zur einfachen Integration eines Online Foto Nachbestellsystems zur Verfuegung
+Version: 1.4.1
+Author: Thomas Schiffler
+Author URI: http://www.Portrait-Service.com/
+*/
+
+ // Session wird benötigt
+ session_start();
+
+ // weitere Referenzen einbinden
+ require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-configure.php';
+ require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-classes.php';
+ require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-connector.php';
+ require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-functions.php';
+ require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-adminarea.php';
+ require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-template-functions.php';
+
+ // Initialisierung und Anlegen der Optionen
+ function pawps_plugin_activate() {
+ 	if ( ! current_user_can( 'activate_plugins' ) )
+ 		return;
+ 	
+ 	add_option(PAWPS_OPTION_HASHKEY, pawps_generateLocalHash());
+ 	add_option(PAWPS_OPTION_HASHKEY_REMOTE);
+ 	add_option(PAWPS_OPTION_USERID);
+ 	add_option(PAWPS_LAST_UPDATE_SHOOTINGS);
+ 	add_option(PAWPS_DB_VERSION, 0);
+ 	add_option(PAWPS_DEBUG, 0);
+
+ 	// Anzeige-Konfiguration
+ 	add_option(PAWPS_TEMPLATE_NAME, 'default');
+ 	add_option(PAWPS_DISPLAY_ROWS, 3);
+ 	add_option(PAWPS_DISPLAY_COLS, 4);
+ 	
+ 	// Create Database
+ 	require_once plugin_dir_path( __FILE__ ) .'/portraitarchiv-setup.php';
+ 	pawps_setupDatabase();
+ 	
+ 	// Schedule Update
+ 	wp_schedule_event(time(), 'daily', 'pawps_refresh_daily_hook');
+ }
+ 
+ // Deaktivierung und Aufräumen
+ function pawps_plugin_deactivate() {
+ 	if ( ! current_user_can( 'activate_plugins' ) )
+ 		return;
+ 	
+ 	// Schedule deaktivieren
+ 	wp_clear_scheduled_hook('pawps_refresh_daily_hook');
+ }
+ 
+  function pawps_insertContent() {
+ 	$pawps_content = get_the_content();
+ 	if (strlen($pawps_content) > 0) {		
+ 		
+ 		// Eventliste
+ 		if (strpos($pawps_content, "[pawps_publicList]") > -1) {
+ 			ob_start();
+ 			pawps_publicEventList();
+ 			$displayContent = ob_get_contents();
+ 			ob_end_clean();
+ 			
+ 			$pawps_content = str_replace("[pawps_publicList]", $displayContent, $pawps_content);
+ 		}
+ 		
+ 		// Einzelevent
+ 		if ((strpos($pawps_content, "[pawps_galerie]") > -1) && (strpos($pawps_content, "[/pawps_galerie]") > -1)) {
+ 			$shootingId = substr($pawps_content, strpos($pawps_content, "[pawps_galerie]") + strlen("[pawps_galerie]"));
+ 			$shootingId = substr($shootingId, 0, strpos($shootingId, "[/pawps_galerie]"));
+ 			if (is_numeric($shootingId)) {
+ 				ob_start();
+ 				pawps_showPublicEvent($shootingId);
+ 				$displayContent = ob_get_contents();
+ 				ob_end_clean();
+ 				
+ 				$pawps_content = str_replace("[pawps_galerie]" . $shootingId . "[/pawps_galerie]", 
+ 						$displayContent, $pawps_content);
+ 			}
+ 		}
+ 		
+ 		// einfache Passworteingabe
+ 		if (strpos($pawps_content, "[pawps_password]") > -1) {
+ 			ob_start();
+ 			pawps_shootingByCode();
+ 			$displayContent = ob_get_contents();
+ 			ob_end_clean();
+ 		
+ 			$pawps_content = str_replace("[pawps_password]", $displayContent, $pawps_content);
+ 		} 		
+ 		
+ 		// Gallericode
+ 		if (strpos($pawps_content, "[pawps_galeriecode]") > -1) {
+ 			ob_start();
+ 			pawps_shootingByGalleriecode();
+ 			$displayContent = ob_get_contents();
+ 			ob_end_clean();
+ 				
+ 			$pawps_content = str_replace("[pawps_galeriecode]", $displayContent, $pawps_content);
+ 		}
+ 	}
+
+ 	return $pawps_content;
+ 	
+ }
+ 
+ function pawps_register_stylesheet() {
+ 	$userTemplate = get_option(PAWPS_TEMPLATE_NAME);
+ 	if (strpos($userTemplate, PAWPS_USERTPL_START) > -1) {
+ 		// Benutzertemplate
+ 		wp_register_style('portrait-archiv-shop', pawps_getUsertemplateDirUrl() . $userTemplate . '/style.css');
+ 	} else {
+ 		wp_register_style('portrait-archiv-shop', plugins_url('portrait-archiv-shop/templates/' . get_option(PAWPS_TEMPLATE_NAME) . '/style.css'));
+ 	}
+ 	
+ 	wp_enqueue_style('portrait-archiv-shop');
+ }
+ 
+
+ // Die aktivieren/deaktivieren Funktionen registrieren
+ add_action('activate_' . plugin_basename(__FILE__),   'pawps_plugin_activate');
+ add_action('deactivate_' . plugin_basename(__FILE__), 'pawps_plugin_deactivate');
+ 
+ // eigentliche Funktionen registrieren einbauen
+ add_action('wp_enqueue_scripts', 'pawps_register_stylesheet');
+ add_action('admin_enqueue_scripts', 'pawps_register_stylesheet');
+ add_filter('the_content', 'pawps_insertContent', 9);
+ 
+ // Regelmäßigen Refresh aktivieren
+ add_action('pawps_refresh_daily_hook', 'pawps_refresh_daily');
+
+ // Menü hinzufügen
+ add_action('admin_menu', 'pawps_admin_menu' );
+ 
+?>
